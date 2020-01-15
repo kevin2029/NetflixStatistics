3441314033010a034524208e12247faa908523a9
Mike <Mikejansen2@icloud.com>
2020-01-15 19:48:14 +0100

    Nieuwe classes aangemaakt waar een ArrayList aangemaakt moet worden door middel een querrie.

---
+++ NetflixStatistics/src/ArrayLists/ArrayLists_Accounts.java
@@ -0,0 +1,36 @@
+package ArrayLists;
+
+import java.util.ArrayList;
+
+public class ArrayLists_Accounts {
+
+    public ArrayList ArraylistAlleAccounts(){
+        //Hier wordt de ArrayList aangemaakt.
+        ArrayList AlleAccounts=new ArrayList();
+
+        //voeg de namen van de accounts toe aan de ArrayList door middel van een for each loop en een querrie
+
+        //Hier wordt de ArrayList gereturned
+        return AlleAccounts;
+    }
+
+    public ArrayList ArrayListAccountsMet1Profiel(){
+        //Hier wordt de ArrayList aangemaakt.
+        ArrayList AccountsMet1Profiel=new ArrayList();
+
+        //Voeg de namen van de accounts met 1 profiel toe aan de ArrayList door middel van een for each loop en een querrie.
+
+        //Hier wordt de ArrayList gereturned.
+        return AccountsMet1Profiel;
+    }
+
+    public ArrayList ArrayList_AccountsMetMeerDan1Profiel(){
+        //Hier wordt de ArrayList aangemaakt.
+        ArrayList AccountMetMeerDan1Profiel=new ArrayList();
+
+        //Voeg de namen van de accounts met meer dan 1 profiel toe aan de ArrayList door middel van een for each loop en een querrie.
+
+        //Hier wordt de ArrayList gereturned.
+        return AccountMetMeerDan1Profiel;
+    }
+}
